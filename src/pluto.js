4283a65d28a26b61cc1152ed9c3fb5eae310caae
Joshua Neuheisel <joshua@neuheisel.us>
2021-05-30 11:18:47 -0400

    initial editor rendering

--- src/pluto.js
+++ src/pluto.js
@@ -1,7 +1,62 @@
 'use strict';
 
+const stream = require('stream');
+const util = require('util');
+const vscode = require('vscode');
+
+const HeaderExtractorTransform = require('../lib/HeaderExtractorTransform');
+const Record = require('../lib/Record');
+const RecordExtractorTransform = require('../lib/RecordExtractorTransform');
+
+function assembleCustomDocument(uri) {
+    const pipeline = util.promisify(stream.pipeline);
+
+    return vscode.workspace.fs.readFile(uri)
+        .then(value => {
+            const fileBytesSource = new stream.Readable({
+                read(size) {
+                    this.push(value);
+                    this.push(null);
+                }
+            });
+
+            const recordCollection = [];
+            const recordCollector = new stream.Writable ({
+                writev: (chunks, callback) => {
+                    chunks.forEach(x => { recordCollection.push(new Record(x.chunk)); });
+                    callback();
+                }
+            });
+
+            const recordPipeline = pipeline(
+                fileBytesSource,
+                new HeaderExtractorTransform(),
+                new RecordExtractorTransform(),
+                recordCollector
+            ).then(() => ({
+                records: recordCollection,
+                uri: uri,
+                dispose: () => {}
+            }));
+
+            return recordPipeline;
+        });
+}
+
+class PlutoEditorProvider {
+    openCustomDocument(uri, openContext, token) {
+        return assembleCustomDocument(uri);
+    }
+
+    resolveCustomEditor(document, webviewPanel, token) {
+        const content = document.records.map(x => x.toString()).join('<br/>');
+        webviewPanel.webview.html = content;
+    }
+}
+
 function activate(context) {
-    console.log('activate');
+    const disposable = vscode.window.registerCustomEditorProvider('pluto.keytab', new PlutoEditorProvider());
+    context.subscriptions.push(disposable);
 }
 
 function deactivate() {}
